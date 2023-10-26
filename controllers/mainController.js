df89b45cfe2676ff63f4dcd1ad35c6dfed3a9bab
0wenB <bryanowen.code@gmail.com>
2023-10-26 09:28:54 +0700

    lg bikin checkout logic belom

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,34 +1,58 @@
-const {Invoice,Medicine,Profile,User} = require('../models')
+const { Invoice, Medicine, Profile, User } = require('../models')
 
-class MainController{
+class MainController {
 
 
-    static async mainPagePatient(req,res){
+    static async mainPagePatient(req, res) {
         try {
             const userProfile = await Profile.findByPk(req.params.idPatient)
             const medicines = await Medicine.findAll()
-            res.render('mainPage', {userProfile, medicines})
+            res.render('mainPage', { userProfile, medicines })
         } catch (error) {
             console.log(error);
             res.send(error)
         }
     }
 
-    static async mainPageAdmin(req,res){
+    static async mainPageAdmin(req, res) {
         try {
-            const userProfile = await Profile.findByPk(req.params.idPatient)
-            const medicines = await Medicine.findAll()
-            res.render('mainPage', {userProfile, medicines})
+
+        } catch (error) {
+            res.send(error)
+        }
+    }
+    static async checkout(req, res) {
+        try {
+            console.log('masuk');
+            const { idPatient } = req.params
+            let data = await Profile.findAll({ include: { all: true, nested: true }, where: { id: idPatient } })
+            console.log(data);
+            res.render('checkout')
+        } catch (error) {
+            console.log(error);
+            res.send(error)
+        }
+    }
+    static async checkoutDestroy(req, res) {
+        try {
+
+        } catch (error) {
+            res.send(error)
+        }
+    }
+    static async thankYouPage(req, res) {
+        try {
+
         } catch (error) {
             res.send(error)
         }
     }
 
-    static async postInvoice(req,res){
+    static async postInvoice(req, res) {
         try {
             const input = {
-                ProfileId : req.params.idPatient,
-                MedicineId : req.params.idMedicine,
+                ProfileId: req.params.idPatient,
+                MedicineId: req.params.idMedicine,
                 quantity: req.body.quantity
             }
             let addedInvoice = await Invoice.create(input)
@@ -36,7 +60,7 @@ class MainController{
             res.redirect(`/main/${req.params.idPatient}?data=${addedInvoice.MedicineId},${addedInvoice.quantity}`)
         } catch (error) {
             res.send(error)
-            
+
         }
     }
 }
